fc0931970cc9200ce055223e0259369fada3d4b4
sharofiddin <pardayev.sharofiddin@gmai.com>
2019-11-26 21:59:30 +0500

    name changed to byteNum

--- Dec2Bin.java
+++ Dec2Bin.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Dec2Bin{
 	public static void main(String... args){
 		Scanner in = new Scanner(System.in);
-		byte num = in .nextByte();
-		String strVal = Integer.toBinaryString(num);
-		System.out.println("Decimal " + num+ " = "+"Binary "+ strVal);
+		byte byteNum = in .nextByte();
+		String strVal = Integer.toBinaryString(byteNum);
+		System.out.println("Decimal " + byteNum+ " = "+"Binary "+ strVal);
 		in.close();
 	}
 }
